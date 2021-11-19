dc29a1fcad39968d72ff56df87a381dbece06fad
vkpindia <infovkpg@gmail.com>
2021-11-19 20:36:33 +0530

    typescript

---
+++ ts-app/app.js
@@ -0,0 +1,3 @@
+function showMessage() {
+    console.log("Welcome TypeScript Programing");
+}
---
+++ ts-app/app.ts
@@ -0,0 +1,17 @@
+/* 
+ * Steps to compile ts fie
+
+1. Create a .ts file
+2. then target the folder in terminal where file is available
+3. then use the command 
+
+tsc filename.ts
+
+4. Ts file will compile and create a new a new file with same name but with .js 
+extension
+5. Then we can use transpiled js file in html code
+*/
+
+function showMessage(){
+    console.log("Welcome TypeScript Programing");
+}
