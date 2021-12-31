064ae1cb9307a7a045bff0080a180f03964c2e33
vkpindia <infovkpg@gmail.com>
2021-12-31 22:00:59 +0530

    compoenet demo

---
+++ angular-demo/src/app/enum/stidudent-record.enum.ts
@@ -0,0 +1,6 @@
+export enum studentData {
+  sid = 1025,
+  sname = 'Smith',
+  course = 'Angular 13',
+  duration = '30 Days'
+}
---
+++ angular-demo/src/app/models/employee.model.ts
@@ -0,0 +1,8 @@
+
+export interface EmployeeT{
+  name: string;
+  empID: number;
+  ejob: string;
+  eSal: number
+  eDept?: string | number | Array<any>
+}
