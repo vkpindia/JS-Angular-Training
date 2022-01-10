c14ddf1f03012d35296e65038a5ac87eb199ecf2
vkpindia <infovkpg@gmail.com>
2022-01-10 21:08:11 +0530

    types

---
+++ typescript-training/types-demo.js
@@ -0,0 +1,11 @@
+var num1 = 20;
+var sid = 1024;
+var sname = 'Smith';
+var course = 'Angular 13';
+var isFeePaid = true;
+var duration = '30 Days';
+console.log('sid', sid);
+console.log('sname', sname);
+console.log('course', course);
+console.log('isFeePaid', isFeePaid);
+console.log('duration', duration);
---
+++ typescript-training/types-demo.ts
@@ -0,0 +1,13 @@
+var num1: any = 20;
+
+var sid: number = 1024;
+let sname: string = 'Smith';
+let course: string = 'Angular 13';
+let isFeePaid: boolean = true;
+var duration: string = '30 Days';
+
+console.log('sid', sid);
+console.log('sname', sname);
+console.log('course', course);
+console.log('isFeePaid', isFeePaid);
+console.log('duration', duration);
