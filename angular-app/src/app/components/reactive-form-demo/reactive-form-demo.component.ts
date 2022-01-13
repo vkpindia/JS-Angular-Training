f8597b728ba96b0c8442a5737cdbdf9c9e507306
vkpindia <infovkpg@gmail.com>
2022-01-13 20:41:16 +0530

    form array

--- angular-app/src/app/components/reactive-form-demo/reactive-form-demo.component.ts
+++ angular-app/src/app/components/reactive-form-demo/reactive-form-demo.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form-demo',
@@ -25,7 +25,7 @@ export class ReactiveFormDemoComponent implements OnInit {
         gender: ['male', [Validators.required]],
         address: this.formBuilder.group({
           addressLine1: ['', [Validators.required]],
-          addressLine2: [''],
+          addressLine: this.formBuilder.array([]),
           landmark: ['', [Validators.required]],
           state: ['', [Validators.required]],
           city: ['', [Validators.required]],
@@ -38,6 +38,25 @@ export class ReactiveFormDemoComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  addAddressFild(){
+    let formGrpup = this.formBuilder.group({
+      addressLine2: ['', [Validators.required]]
+    });
+
+    let formArray = (<FormArray>(<FormGroup>this.userForm.get('address')).get('addressLine'));
+    formArray.push(formGrpup);
+    console.log('formArray', formArray.value);
+  }
+
+  removeField(index:number){
+    let formArray = (<FormArray>(<FormGroup>this.userForm.get('address')).get('addressLine'));
+    formArray.removeAt(index);
+  }
+
+  get addressArray(){
+    return (<FormArray>(<FormGroup>this.userForm.get('address')).get('addressLine'));
+  }
+
   get formControl(){
     return this.userForm.controls;
   }
@@ -47,10 +66,14 @@ export class ReactiveFormDemoComponent implements OnInit {
   }
 
   saveData(){
-    console.log('userForm', this.userForm.value);
     if(this.userForm.invalid){
       this.userForm.markAllAsTouched();
       return;
+    } else {
+      alert(JSON.stringify(this.userForm.value));
+      /* this.userForm.patchValue(this.userForm.value);
+      this.userForm.setValue(this.userForm.value); */
+      this.userForm.reset();
     }
   }
 
